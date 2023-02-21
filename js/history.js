e4c74ace413ecbe566bc423df95d8117873a30eb
Harsh Bhandari <harshb@sahaj.ai>
2023-02-22 00:17:58 +0530

    Show order history in table

--- js/history.js
+++ js/history.js
@@ -27,18 +27,76 @@ form.addEventListener("submit", (event) => {
         );
         errorMessage.appendChild(list);
       } else {
-        const title = document.createElement("p");
-        title.innerText = "Order History";
-        successMessage.appendChild(title);
-        for (key in data) {
-          const listElement = document.createElement("li");
-          listElement.innerText = `${key}: ${JSON.stringify(data[key])}`;
-          list.appendChild(listElement);
+        const message = document.createElement("p");
+        if (data.length == 0) {
+          message.innerText = "No orders placed";
+          successMessage.appendChild(message);
+        } else {
+          successMessage.appendChild(message);
+          message.innerText = "Order History";
+          const table = createTable(data);
+          successMessage.appendChild(table);
         }
-        successMessage.appendChild(list);
       }
     })
     .catch((reason) => {
       console.log(reason);
     });
 });
+
+function createTable(data) {
+  const table = document.createElement("table");
+  const headings = [
+    "Order ID",
+    "Order Type",
+    "Quantity",
+    "Price",
+    "ESOP Type",
+    "Remaining Quanity",
+    "Status",
+    "Filled",
+  ];
+  const keys = [
+    "orderId",
+    "type",
+    "quantity",
+    "price",
+    "esopType",
+    "remainingQuantity",
+    "status",
+    "filled",
+  ];
+  const tableHeadingRow = document.createElement("tr");
+  headings.forEach((value) => {
+    const tableHeading = document.createElement("th");
+    tableHeading.innerText = value;
+    tableHeadingRow.appendChild(tableHeading);
+  });
+  table.appendChild(tableHeadingRow);
+  for (row of data) {
+    const tableRow = document.createElement("tr");
+    keys.forEach((key) => {
+      const tableRowData = document.createElement("td");
+      console.log(row, row[key]);
+      if (row[key] == undefined) {
+        tableRowData.innerText = "-";
+      } else if (key == "filled") {
+        tableRowData.append(...createFilledList(row[key]));
+      } else {
+        tableRowData.innerText = row[key];
+      }
+      tableRow.appendChild(tableRowData);
+    });
+    table.appendChild(tableRow);
+  }
+  return table;
+}
+
+function createFilledList(filled) {
+  return filled.map(({ quantity, price }) => {
+    const element = document.createElement("p");
+    element.innerText = `Quantity: ${quantity} Price: ${price}`;
+    console.log("twicd");
+    return element;
+  });
+}
