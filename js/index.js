0390040fd86202d8639bb3e2193a24d2cc794c56
Pranav-Bhosale <pranavbhosale@sahaj.ai>
2023-02-21 16:02:08 +0530

    Display Error message

--- js/index.js
+++ js/index.js
@@ -1,15 +1,18 @@
 const form = document.querySelector("#form");
-
+const successMessage = document.querySelector("#successMessage");
+const errorMessage = document.querySelector("#errorMessage");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const { username, orderType, quantity, price, esopType } =
     event.target.elements;
   const reqBody = {
     type: orderType.value,
-    price: price.value,
+    price: parseInt(price.value),
     esopType: esopType.value,
-    quantity: quantity.value,
+    quantity: parseInt(quantity.value),
   };
+  successMessage.innerHTML = ""
+  errorMessage.innerHTML = ""
   fetch(`http://localhost:8080/user/${username.value}/order`, {
     method: "POST",
     body: JSON.stringify(reqBody),
@@ -21,6 +24,17 @@ form.addEventListener("submit", (event) => {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
+      const list = document.createElement("ul")
+      if (data.error) {
+        list.append(
+          data.error.map(element => {
+          return document.createElement("li").innerText = element
+        }));
+        errorMessage.appendChild(list);
+      } else {
+         list.append(document.createElement("li").innerText = "Order Placed Successfully")
+         successMessage.appendChild(list);
+      }
     })
     .catch((reason) => {
       console.log(reason);
