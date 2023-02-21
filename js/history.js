9efef1309d7572582d0275b8efd84afb425c00c4
Harsh Bhandari <harshb@sahaj.ai>
2023-02-21 22:23:25 +0530

    Format files

--- js/history.js
+++ js/history.js
@@ -3,10 +3,9 @@ const successMessage = document.querySelector("#successMessage");
 const errorMessage = document.querySelector("#errorMessage");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const { username } =
-    event.target.elements;
-  successMessage.innerHTML = ""
-  errorMessage.innerHTML = ""
+  const { username } = event.target.elements;
+  successMessage.innerHTML = "";
+  errorMessage.innerHTML = "";
   fetch(`http://localhost:8080/user/${username.value}/order`, {
     method: "GET",
     headers: {
@@ -17,25 +16,25 @@ form.addEventListener("submit", (event) => {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      const list = document.createElement("ul")
+      const list = document.createElement("ul");
       if (data.error) {
         list.append(
-          ...data.error.map(element => {
-          const listElement = document.createElement("li")
-          listElement.innerText = element
-          return listElement
-        }));
+          ...data.error.map((element) => {
+            const listElement = document.createElement("li");
+            listElement.innerText = element;
+            return listElement;
+          })
+        );
         errorMessage.appendChild(list);
       } else {
-        const title=document.createElement("p")
-        title.innerText="Order History"
+        const title = document.createElement("p");
+        title.innerText = "Order History";
         successMessage.appendChild(title);
-        for(key in data)
-        {
-           const listElement=document.createElement("li")
-           listElement.innerText=`${key}: ${JSON.stringify(data[key])}`
-           list.appendChild(listElement)
-        }          
+        for (key in data) {
+          const listElement = document.createElement("li");
+          listElement.innerText = `${key}: ${JSON.stringify(data[key])}`;
+          list.appendChild(listElement);
+        }
         successMessage.appendChild(list);
       }
     })
