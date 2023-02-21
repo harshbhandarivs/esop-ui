b1d477b160c4d9b848256f115bea4b886270c365
Pranav-Bhosale <pranavbhosale@sahaj.ai>
2023-02-21 18:33:33 +0530

    toggle esopType based on OrderType

--- js/index.js
+++ js/index.js
@@ -1,19 +1,49 @@
 const form = document.querySelector("#form");
 const successMessage = document.querySelector("#successMessage");
 const errorMessage = document.querySelector("#errorMessage");
+const orderTypeSelector=document.querySelector("#orderType");
+const esopTypeSelector=document.querySelector("#esopType");
+
+orderTypeSelector.addEventListener("change",(event)=>{
+    if(event.target.value=="SELL")
+    {
+            esopTypeSelector.style.display="block"
+    }
+    else{
+      esopTypeSelector.style.display="none"
+    }
+});
+ 
 form.addEventListener("submit", (event) => {
+  
   event.preventDefault();
-  const { username, orderType, quantity, price, esopType } =
-    event.target.elements;
+  const reqBody= createRequestBody(event)
+  placeOrder(reqBody,event)
+});
+
+
+function createRequestBody(event)
+{
+  const { orderType, quantity, price, esopType } =event.target.elements;
   const reqBody = {
     type: orderType.value,
     price: parseInt(price.value),
-    esopType: esopType.value,
     quantity: parseInt(quantity.value),
   };
+  if(orderType.value=="SELL")
+  {
+    reqBody["esopType"]=esopType.value
+  }
+  return reqBody
+}
+
+function placeOrder(reqBody,event)
+{
   successMessage.innerHTML = ""
   errorMessage.innerHTML = ""
-  fetch(`http://localhost:8080/user/${username.value}/order`, {
+  const {userName}=event.target.elements
+
+  fetch(`http://localhost:8080/user/${userName.value}/order`, {
     method: "POST",
     body: JSON.stringify(reqBody),
     headers: {
@@ -23,22 +53,47 @@ form.addEventListener("submit", (event) => {
   })
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       const list = document.createElement("ul")
       if (data.error) {
-        list.append(
-          ...data.error.map(element => {
-          const listElement = document.createElement("li")
-          listElement.innerText = element
-          return listElement
-        }));
-        errorMessage.appendChild(list);
+        appendErrorMessage(list,data.error)
       } else {
-         list.append(document.createElement("li").innerText = "Order Placed Successfully")
-         successMessage.appendChild(list);
+        appendOrderSummary(list,data)
       }
     })
     .catch((reason) => {
       console.log(reason);
     });
-});
+}
+
+function appendErrorMessage(list,error)
+{
+  const title=document.createElement("p")
+  title.innerText="Errors =>"
+  errorMessage.appendChild(title);
+  list.append(
+    ...error.map(element => {
+    const listElement = document.createElement("li")
+    listElement.innerText = element
+    return listElement
+  }));
+  errorMessage.appendChild(list);
+}
+
+function appendOrderSummary(list,data)
+{
+  const message=document.createElement("p")
+        message.innerText="Order Placed Successfully!!"
+        const title=document.createElement("p")
+        title.innerText="Order Summary =>"
+        successMessage.append(message,title);
+        for(key in data)
+        {
+          if(key=="orderID"||key=="type"||key=="quantity"||key=="price"||(key=="esopType"&&data["type"]=="SELL"))
+          {
+           const listElement=document.createElement("li")
+           listElement.innerText=`${key}: ${data[key]}`
+           list.appendChild(listElement)
+         }
+        }          
+        successMessage.appendChild(list); 
+}
